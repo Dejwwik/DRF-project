d1d8cc433eb3fecc3fd8b5f5274282c375dd575b
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-15 21:00:24 +0100

    Update URL patterns
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,7 @@
-from .views import product_detail_view, product_create_list_view
+from .views import product_detail_view, product_create_list_view, product_alt_view
 from django.urls import path
 
 urlpatterns = [
-    path("<pk>/", product_detail_view, name="product_detail"),
+    path("<pk>/", product_alt_view),
     path("", product_create_list_view, name="product_create_list")
 ]
