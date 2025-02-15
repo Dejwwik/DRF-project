2d96a16e64b8e83c9e345aff81bee80a4f109e84
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-15 20:45:47 +0100

    Content is set to title if it's not provided.
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- backend/products/views.py
+++ backend/products/views.py
@@ -14,6 +14,11 @@ class ProductCreateApiView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def perform_create(self, serializer: ProductSerializer):
+        content = serializer.validated_data.get("content")
+        if not content:
+            content = serializer.validated_data["title"]
+        serializer.save(content=content)
 
 product_detail_view = ProductDetailApiView.as_view()
 product_create_view = ProductCreateApiView.as_view()
--- py_client/create.py
+++ py_client/create.py
@@ -3,13 +3,12 @@ import requests
 
 endpoint = "http://localhost:8000/api/products/"
 
-# Raise validation error
-resp = requests.post(endpoint)
-print(resp.json())
+# # Raise validation error
+# resp = requests.post(endpoint)
+# print(resp.json())
 
 data = {
     "title" : "create_title",
-    "content" : "create_content",
     "price" : random.randint(0, 100) + round(random.randint(1, 99) / 100, 2)
 }
 resp = requests.post(endpoint, data=data)
