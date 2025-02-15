aecd34f2053c5af02fed6fb5a5f4484f0cd2d090
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-15 18:23:05 +0100

    Add Product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.TextField(max_length=120)
+    content = models.TextField(max_length=500, null=True, blank=True)
+    price = models.DecimalField(max_digits=15, decimal_places=5)
+
