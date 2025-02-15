00b35a47ae5b87ddcfb8da10b0668161e13b8ec4
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-15 20:39:06 +0100

    Add script to create products via API endpoint

---
+++ py_client/create.py
@@ -0,0 +1,17 @@
+import random
+
+import requests
+
+endpoint = "http://localhost:8000/api/products/"
+
+# Raise validation error
+resp = requests.post(endpoint)
+print(resp.json())
+
+data = {
+    "title" : "create_title",
+    "content" : "create_content",
+    "price" : random.randint(0, 100) + round(random.randint(1, 99) / 100, 2)
+}
+resp = requests.post(endpoint, data=data)
+print(resp.json())
