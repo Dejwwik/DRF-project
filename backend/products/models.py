dee60aa8aecdabf463491bce478aaee44b831b70
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-16 15:06:52 +0100

    Add user to product.
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
 
 class Product(models.Model):
     title = models.TextField(max_length=120)
     content = models.TextField(max_length=500, null=True, blank=True)
     price = models.DecimalField(max_digits=15, decimal_places=5)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="products"
+    )
 
     @property
     def sale_price(self):
-        return "%.2f" %(float(self.price) * 0.8)
+        return "%.2f" % (float(self.price) * 0.8)
 
     def get_discount(self) -> float:
         return int(self.price) * 0.1
