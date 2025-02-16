d9d6d8d1ca9c369a63d14a36c8ab5382ad376447
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-16 11:47:25 +0100

    Add authorization into the py-client create.py
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- py_client/create.py
+++ py_client/create.py
@@ -1,15 +1,27 @@
+import os
 import random
 import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 endpoint = "http://localhost:8000/api/products/"
+auth_endpoint = "http://localhost:8000/api/auth/"
 
-# # Raise validation error
-# resp = requests.post(endpoint)
-# print(resp.json())
+auth_response = requests.post(auth_endpoint,
+    data = {
+        "username" : os.environ.get("USERNAME"),
+        "password": os.environ.get("PASSWORD")
+    }
+)
+if auth_response.status_code == 200:
+    token = auth_response.json()["token"]
 
-data = {
-    "title" : "create_title",
-    "price" : random.randint(0, 100) + round(random.randint(1, 99) / 100, 2)
-}
-resp = requests.post(endpoint, data=data)
-print(resp.json())
+
+    resp = requests.post(endpoint,
+            headers = {"Authorization":f"Token {token}"},
+        data = {
+            "title" : "create_title",
+            "price" : random.randint(0, 100) + round(random.randint(1, 99) / 100, 2)
+        }
+    )
+    print(resp.json())
