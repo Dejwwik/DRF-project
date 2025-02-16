1eb9e02a7c30f3fe8ece4ec652816e7115e98c6a
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-16 15:07:04 +0100

    Update urls with name
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -7,8 +7,8 @@ from .views import (
 from django.urls import path
 
 urlpatterns = [
-    path("", product_create_list_view),
+    path("", product_create_list_view, name="product-list"),
     path("<pk>/", product_detail_view, name="product-detail"),
-    path("<pk>/update/", product_update_view),
-    path("<pk>/delete/", product_destroy_view),
+    path("<pk>/update/", product_update_view, name="product-update"),
+    path("<pk>/delete/", product_destroy_view, name="product-delete"),
 ]
