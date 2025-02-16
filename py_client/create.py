72d3ab2f344300cdf3dba693501d481dc8f9b1cc
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-16 11:50:23 +0100

    Test formatting
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- py_client/create.py
+++ py_client/create.py
@@ -7,21 +7,22 @@ load_dotenv()
 endpoint = "http://localhost:8000/api/products/"
 auth_endpoint = "http://localhost:8000/api/auth/"
 
-auth_response = requests.post(auth_endpoint,
-    data = {
-        "username" : os.environ.get("USERNAME"),
-        "password": os.environ.get("PASSWORD")
-    }
+auth_response = requests.post(
+    auth_endpoint,
+    data={
+        "username": os.environ.get("USERNAME"),
+        "password": os.environ.get("PASSWORD"),
+    },
 )
 if auth_response.status_code == 200:
     token = auth_response.json()["token"]
 
-
-    resp = requests.post(endpoint,
-            headers = {"Authorization":f"Token {token}"},
-        data = {
-            "title" : "create_title",
-            "price" : random.randint(0, 100) + round(random.randint(1, 99) / 100, 2)
-        }
+    resp = requests.post(
+        endpoint,
+        headers={"Authorization": f"Token {token}"},
+        data={
+            "title": "create_title",
+            "price": random.randint(0, 100) + round(random.randint(1, 99) / 100, 2),
+        },
     )
     print(resp.json())
