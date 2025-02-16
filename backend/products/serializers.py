f3d60dde47dd9f08e8196b0f14b90e4f51d55b9c
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-16 16:19:53 +0100

    Serializer cleanup
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,16 +1,13 @@
 from rest_framework import serializers
-from rest_framework.reverse import reverse
 from .models import Product
 from .validations import validate_title
 from api.serializers import UserPublicSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    discount = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
         view_name="product-detail", lookup_field="pk"
     )
-    edit_url = serializers.SerializerMethodField(read_only=True)
     title = serializers.CharField(validators=[validate_title])
     owner = UserPublicSerializer(read_only=True, source="user")
 
@@ -19,20 +16,8 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "owner",
-            "edit_url",
             "url",
             "content",
             "title",
             "price",
-            "sale_price",
-            "discount",
         )
-
-    def get_edit_url(self, obj):
-        request = self.context.get("request")
-        if request is None:
-            return None
-        return reverse("product-update", kwargs={"pk": obj.pk}, request=request)
-
-    def get_discount(self, obj):
-        return obj.get_discount()
