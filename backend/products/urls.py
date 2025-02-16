d0f88a63e2b277d173cf873ee6135ad36528b77b
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-16 13:55:49 +0100

    Add url to product serialiazer
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,14 @@
-from .views import product_detail_view, product_create_list_view, product_update_view, product_destroy_view
+from .views import (
+    product_detail_view,
+    product_create_list_view,
+    product_update_view,
+    product_destroy_view,
+)
 from django.urls import path
 
 urlpatterns = [
     path("", product_create_list_view),
-    path("<pk>/", product_detail_view),
+    path("<pk>/", product_detail_view, name="product-detail"),
     path("<pk>/update/", product_update_view),
     path("<pk>/delete/", product_destroy_view),
 ]
