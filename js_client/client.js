4eb5e51ebf1ff29a21e6f3e2c0715fda475b4038
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-17 16:22:36 +0100

    Add search form and implement search functionality in client.js
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- js_client/client.js
+++ js_client/client.js
@@ -1,8 +1,13 @@
 const loginForm = document.getElementById("login-form")
+const searchForm = document.getElementById("search-form")
 const baseEndpoint = "http://localhost:8000/api"
 const contentContainer = document.getElementById("content-container")
+
 if (loginForm) {
     loginForm.addEventListener('submit', handleLogin)
+}
+if (searchForm) {
+    searchForm.addEventListener('submit', handleSearch)
 }
 
 function handleLogin(event) {
@@ -27,21 +32,55 @@ function handleLogin(event) {
     })
 }
 
+function handleSearch(event) {
+    event.preventDefault()
+
+    let formData = new FormData(searchForm)
+    let data = Object.fromEntries(formData)
+    let searchParams = new URLSearchParams(data)
+    const endpoint = baseEndpoint + "/search/?" + searchParams
+    const headers = {
+        "Content-Type": "application/json",
+    }
+    const authToken = localStorage.getItem("access")
+    if (authToken){
+        headers["Authorization"] = "Bearer" + authToken
+    }
+
+    const options = {
+        method: "GET",
+        headers: headers
+    }
+    fetch(endpoint, options).then(response => {
+        return response.json()
+    }).then(data => {
+        console.log(data.hits)
+        writeToContainer(data)
+    }).catch(err => {
+        console.log('error', err)
+    })
+}
+
 function writeToContainer(data){
     if (contentContainer) {
         contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4) + "</pre>"
     }
 }
 
-function getFetchOptions(method, body){
-    return {
-        method: method === null ? "GET" : method,
+function getFetchOptions(method, body) {
+    const options = {
+        method: method || "GET",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem("access")}`
-        },
-        body: body ? body : null
+            "Authorization": "Bearer " + localStorage.getItem("access")
+        }
+    };
+
+    if (body && method !== "GET") {
+        options.body = JSON.stringify(body);
     }
+
+    return options;
 }
 
 function handleAuthData(authData, callback) {
