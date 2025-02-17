157f112452589e86fac800ebc3a138a06c78ba02
Lukáš Kožušník <lukinkozusnik@seznam.cz>
2025-02-17 16:01:13 +0100

    Add content container and enhance authentication handling in client.js
    Signed-off-by: Lukáš Kožušník <lukinkozusnik@seznam.cz>

--- js_client/client.js
+++ js_client/client.js
@@ -1,10 +1,9 @@
 const loginForm = document.getElementById("login-form")
 const baseEndpoint = "http://localhost:8000/api"
-
+const contentContainer = document.getElementById("content-container")
 if (loginForm) {
     loginForm.addEventListener('submit', handleLogin)
 }
-
 
 function handleLogin(event) {
     console.log(event)
@@ -20,11 +19,43 @@ function handleLogin(event) {
         body: JSON.stringify(loginObjectData),
     }
     fetch(loginEndpoint, options).then(response => {
-        console.log(response.body)
         return response.json()
-    }).then(x => {
-        console.log(x)
+    }).then(authData => {
+        handleAuthData(authData, getProductList)
     }).catch(err => {
         console.log('error', err)
     })
+}
+
+function writeToContainer(data){
+    if (contentContainer) {
+        contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4) + "</pre>"
+    }
+}
+
+function getFetchOptions(method, body){
+    return {
+        method: method === null ? "GET" : method,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("access")}`
+        },
+        body: body ? body : null
+    }
+}
+
+function handleAuthData(authData, callback) {
+    localStorage.setItem("access", authData.access)
+    localStorage.setItem("refresh", authData.refresh)
+    if (callback){
+        callback()
+    }
+}
+
+function getProductList(){
+    const endpoint = `${baseEndpoint}/products/`
+    const options = getFetchOptions()
+    fetch(endpoint, options)
+        .then(response => response.json())
+        .then(data => writeToContainer(data))
 }
